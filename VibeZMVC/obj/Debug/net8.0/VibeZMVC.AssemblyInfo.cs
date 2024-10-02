2fc8a92153e9610a19797adde80ef57540e47109
lken1514 <kienl8890@gmail.com>
2024-10-02 09:23:51 +0700

    Update

--- VibeZMVC/obj/Debug/net8.0/VibeZMVC.AssemblyInfo.cs
+++ VibeZMVC/obj/Debug/net8.0/VibeZMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VibeZMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0871e9ce4d933aeccd09966682f3bfac0307c850")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8485d8f2fe2df17eef0fc5920a04749861f694ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("VibeZMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VibeZMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
